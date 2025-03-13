687503566a89ce66164104b3584a1322e0149cd9
Yilay Firstenberg <yfirsten@uci.edu>
2025-03-13 01:30:13 +0000

    Wrote gtests for dijkstras to be able to test as a implement it.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -1,8 +1,62 @@
 #include <gtest/gtest.h>
 
-#include "dijkstras.h"
-#include "ladder.h"
+#include "../src/dijkstras.h"
+#include "../src/ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(DijkstraTest, basic_dk) {
+  Graph G;
+  G.numVertices = 4;
+  G.resize(4);
+  G[0] = {{0, 1, 1}, {0, 2, 4}};
+  G[1] = {{1, 2, 2}, {1, 3, 6}};
+  G[0] = {{2, 3, 3}};
+  G[0] = {};
+
+  vector<int> prev;
+  vector<int> dist = dijkstra_shortest_path(G, 0, prev);
+
+  EXPECT_EQ(dist[0], 0);
+  EXPECT_EQ(dist[1], 1);
+  EXPECT_EQ(dist[2], 3);
+  EXPECT_EQ(dist[3], 6);
+
+  vector<int> path = extract_shortest_path(prev, 3);
+  vector<int> expected_path = {0, 1, 2, 3};
+  EXPECT_EQ(path, expected_path);
+}
+
+TEST(DijkstraTest, disconnected_dk) {
+  Graph G;
+  G.numVertices = 4;
+  G.resize(4);
+  G[0] = {{0, 1, 1}};
+  G[1] = {};
+  G[0] = {{2, 3, 2}};
+  G[0] = {};
+
+  vector<int> prev;
+  vector<int> dist = dijkstra_shortest_path(G, 0, prev);
+
+  EXPECT_EQ(dist[0], 0);
+  EXPECT_EQ(dist[1], 1);
+  EXPECT_EQ(dist[2], INF);
+  EXPECT_EQ(dist[3], INF);
+
+  vector<int> path = extract_shortest_path(prev, 3);
+  EXPECT_TRUE(path.empty());
+}
+
+TEST(DijkstraTest, single_dk) {
+  Graph G;
+  G.numVertices = 1;
+  G.resize(1);
+
+  vector<int> prev;
+  vector<int> dist = dijkstra_shortest_path(G, 0, prev);
+
+  EXPECT_EQ(dist[0], 0);
+
+  vector<int> path = extract_shortest_path(prev, 3);
+  vector<int> expected_path = {0};
+  EXPECT_EQ(path, expected_path);
 }
---
+++ src/dijkstras.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <stack>
+using namespace std;
+#include "dijktras.h"
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+
+}
+
+void print_path(const vector<int>& v, int total) {
+    
+}
