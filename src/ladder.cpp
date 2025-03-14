434439190351901fa3347fda2ec15627c268761a
Yilay Firstenberg <yfirsten@uci.edu>
2025-03-14 23:28:22 +0000

    Implemented generate_word_ladder based off of the provided pseudocode.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -22,7 +22,29 @@ bool is_adjacent(const string& word1, const string& word2) {
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for (const string & word : word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (visited.find(word) == visited.end()) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word) {
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
