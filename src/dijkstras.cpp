88b2dc77f270c724498f2f9947f57301851dc9d9
Yilay Firstenberg <yfirsten@uci.edu>
2025-03-15 03:06:00 +0000

    Added and endl to dijkstras print.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -55,5 +55,6 @@ void print_path(const vector<int>& v, int total) {
     for (int i = 0; i < v.size(); ++i) {
         cout << v[i] << " ";
     }
+    cout << endl;
     cout << "Total cost is " << total << endl;
 }
