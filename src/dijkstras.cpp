0616491177270ff0e28d61d2c08a04bb41065eba
Yilay Firstenberg <yfirsten@uci.edu>
2025-03-15 03:03:24 +0000

    Adjusted dijkstras print.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -53,7 +53,7 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 
 void print_path(const vector<int>& v, int total) {
     for (int i = 0; i < v.size(); ++i) {
-        cout << v[i];
+        cout << v[i] << " ";
     }
-    cout << total << endl;
+    cout << "Total cost is " << total << endl;
 }
