8d7c55fcebc480f49c3ec642e4bd49599fc373fd
Yilay Firstenberg <yfirsten@uci.edu>
2025-03-15 02:56:43 +0000

    Made some final (hopefully) adjustments.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -13,71 +13,6 @@ void error(string word1, string word2, string msg) {
     cerr << "Error: " << msg << word1 << word2 << endl;
 }
 
-map<string, vector<string>> preprocess(const set<string> & word_list) {
-    map<string, vector<string>> adj_map;
-
-    for (const string & word : word_list) {
-        for (size_t i = 0; i < word.length(); ++i) {
-            string pat = word;
-            pat[i] = '*';
-            adj_map[pat].push_back(word);
-        }
-
-        for (size_t i = 0; i <= word.length(); ++i) {
-            string pat = word.substr(0, i) + '*' + word.substr(i);
-            adj_map[pat].push_back(word);
-        }
-    }
-
-    return adj_map;
-}
-
-vector<string> get_adj(const string & word, const map<string, vector<string>> & adj_map) {
-    vector<string> adj_words;
-
-    for (size_t i = 0; i < word.length(); ++i) {
-        string pat = word;
-        pat[i] = '*';
-
-        if (adj_map.count(pat) > 0) {
-            const vector<string> & potential = adj_map.at(pat);
-            for (const string & pot : potential) {
-                if (pot != word) {
-                    adj_words.push_back(pot);
-                }
-            }
-        }
-    }
-
-    for (size_t i = 0; i < word.length(); ++i) {
-        string pat = word.substr(0, i) + '*' + word.substr(i);
-
-        if (adj_map.count(pat) > 0) {
-            const vector<string> & potential = adj_map.at(pat);
-            for (const string & pot : potential) {
-                if (pot.length() == word.length() - 1) {
-                    adj_words.push_back(pot);
-                }
-            }
-        }
-    }
-
-    for (size_t i = 0; i < word.length(); ++i) {
-        string pat = word.substr(0, i) + '*' + word.substr(i + 1);
-
-        if (adj_map.count(pat) > 0) {
-            const vector<string> & potential = adj_map.at(pat);
-            for (const string & pot : potential) {
-                if (pot.length() == word.length() + 1) {
-                    adj_words.push_back(pot);
-                }
-            }
-        }
-    }
-
-    return adj_words;
-}
-
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int len1 = str1.length();
     int len2 = str2.length();
@@ -127,7 +62,6 @@ bool is_adjacent(const string& word1, const string& word2) {
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    //map<string, vector<string>> adj_map = preprocess(word_list);
     queue<vector<string>> ladder_queue;
     ladder_queue.push({begin_word});
     set<string> visited;
@@ -136,7 +70,6 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         vector<string> ladder = ladder_queue.front();
         ladder_queue.pop();
         string last_word = ladder.back();
-        //vector<string> adj_words = get_adj(last_word, adj_map);
         for (const string & word : word_list) {
             if (is_adjacent(last_word, word)){
                 if (visited.find(word) == visited.end()) {
